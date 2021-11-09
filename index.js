e922d0846275445db652e2011f496ecf38565df9
tauhid102 <tauhidurr102@gmail.com>
2021-11-09 17:51:54 +0600

    update

--- index.js
+++ index.js
@@ -3,22 +3,38 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-const port = process.env.PORT  || 5000;
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jfvuq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
-    try{
-        await client.connect();
-        console.log('connect');
-    }
-    finally{
-        // await client.close();
-    }
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('doctors_portal');
+    const appointmentsCollection = database.collection('appointments');
+
+    app.get('/appointments',async(req,res)=>{
+      const email=req.query.email;
+      const date=new Date(req.query.date).toLocaleDateString();
+      const query={email:email,date:date}
+      const cursor=appointmentsCollection.find(query);
+      const appointments= await cursor.toArray();
+      res.json(appointments);
+    })
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment)
+      res.json(result);
+    })
+  }
+  finally {
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
